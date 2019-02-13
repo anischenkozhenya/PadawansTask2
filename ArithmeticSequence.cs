b48689f7d1c1c1b0692d319b212642731c791db7
anischenkozhenya <anischenkozhenya@gmail.com>
2019-02-13 22:11:55 +0300

    Update ArithmeticSequence.cs

--- ArithmeticSequence.cs
+++ ArithmeticSequence.cs
@@ -6,8 +6,24 @@ namespace PadawansTask2
     {
         public static int Calculate(int number, int add, int count)
         {
-            // put your code here
-            throw new NotImplementedException();
+           
+            if(count<1)
+            {
+                throw new NotImplementedException();
+            }
+            int result=0;
+            for (int a=0;a<count;a++)
+            {
+                result += number;
+                if (a > 0)
+                {
+                    for (int i=0;i<a;i++)
+                    {
+                        result += add;
+                    }
+                }
+            }
+            return result;
         }
     }
 }
